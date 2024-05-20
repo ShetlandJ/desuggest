02fdf5e391c1dc12bdab3d58536ff1c8e48738e2
James <electionnightspecial@gmail.com>
2024-05-20 20:38:29 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Fresh Chrome Extension boilerplate (Manifest V3)
+# LinkedIn Desuggest
 
-## Purpose
+If you get nothing of value from the "Suggested" posts on LinkedIn, this Chrome extension is for you!
 
-This extension can be used as a boilerplate for creating new extensions for Chrome based browsers, like Google Chrome.
+To install:
 
-## What it does
-
-When installed it will write something to DevTools console (F12) at the three most common events: before page load, after page load and when the user clicks the extension button on browser top bar.
-# desuggest
-# desuggest
+1. Visit chrome:extensions
+2. Enable Developer mode (top right)
+3. Download this repository as a zip (https://github.com/ShetlandJ/desuggest/archive/refs/heads/main.zip)
+4. Unzip as a folder somewhere on your desktop etc
+5. Back on chrome:extensions, select "Load unpacked" and select the folder itself.
---
+++ desuggest.js
@@ -0,0 +1,36 @@
+(function () {
+  function hideSuggestedTextBlocks(article) {
+    if (article.innerText === 'Suggested') {
+      let outerWrapper = article.closest('.feed-shared-update-v2.feed-shared-update-v2--minimal-padding.full-height.relative');
+      if (outerWrapper) {
+        outerWrapper.style.display = 'none';
+      }
+    }
+  }
+
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        hideSuggestedTextBlocks(entry.target);
+      }
+    });
+  });
+
+  let suggestedTextBlocks = Array.from(document.getElementsByClassName('update-components-text-view break-words'));
+
+  suggestedTextBlocks.forEach(article => {
+    observer.observe(article);
+  });
+
+  let mutationObserver = new MutationObserver((mutations) => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType === 1 && node.classList.contains('update-components-text-view') && node.classList.contains('break-words')) {
+          observer.observe(node);
+        }
+      });
+    });
+  });
+
+  mutationObserver.observe(document.body, { childList: true, subtree: true });
+})();
